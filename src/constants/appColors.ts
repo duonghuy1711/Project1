422221a9cbdc689e2e620b4e8e3c3c70272a5041
Dương Quốc Huy <duongquochuy1103@gmail.com>
2025-03-24 23:33:41 +0700

    add input component

--- src/constants/appColors.ts
+++ src/constants/appColors.ts
@@ -2,8 +2,9 @@ export const appColors = {
     primary: '#5669FF',
     white: '#ffff',
     text: '#120D26',
-    gray: '#3807A7A',
+    gray: '#807A7A',
     gray2: '#DADADA',
+    gray3: '#E4DFDF',
     link: '#5669FF',
 };
 
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,20 +1,38 @@
 import {View, Text} from 'react-native';
-import React from 'react';
-import {ButtonComponent} from '../../components';
-import { globalStyles } from '../../styles/globalStyles';
+import React, {useState} from 'react';
+import {ButtonComponent, InputComponent} from '../../components';
+import {globalStyles} from '../../styles/globalStyles';
+import {appColors} from '../../constants/appColors';
+import {Lock, Sms} from 'iconsax-react-native';
 
 const LoginScreen = () => {
-  return (
-    <View style = {[globalStyles.container, {padding: 16}]}>
-      <Text>LoginScreen</Text>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-      <ButtonComponent
-        text="LOGIN"
-        onPress={() => console.log('login')}
-        type = 'link'
-        icon={<View> 
-          <Text>N</Text>
-        </View>}
+  return (
+    <View
+      style={[
+        globalStyles.container,
+        {justifyContent: 'center', alignItems: 'center', padding: 20},
+      ]}>
+      <InputComponent
+        value={email}
+        placeholder="Email"
+        onChange={val => setEmail(val)}
+        suffix={false} // Example suffix
+        allowClear
+        affix={<Sms size={22} color={appColors.gray} />}
+        // isPassword
+      />
+
+      <InputComponent
+        value={password}
+        placeholder="Password"
+        onChange={val => setPassword(val)}
+        suffix={false} // Example suffix
+        allowClear
+        affix={<Lock size={22} color={appColors.gray} />}
+        isPassword
       />
     </View>
   );
